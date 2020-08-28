a449175eef6e587be1f834ebbe43182cfbc9a819
normidar <1106408182@qq.com>
2020-08-28 18:25:41 +0900

    Initial commit

---
+++ lib/fig_accounting_model.dart
@@ -0,0 +1,5 @@
+library fig_accounting_model;
+
+import 'package:fig_accounting_model/fig_accounting_model.dart';
+
+
---
+++ test/fig_accounting_model_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:fig_accounting_model/fig_accounting_model.dart';
+
+void main() {
+  test('adds one to input values', () {
+    
+  });
+}
